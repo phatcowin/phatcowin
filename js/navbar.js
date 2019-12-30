58edc0dfc8aa232eb61479c60c879e1bfd920458
Dalton Cowin <dcowin1998@gmail.com>
2019-12-29 18:25:29 -0800

    Start of newsletter decoration

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-// Navigation Bar
-window.onscroll = function() {
-    var prevScroll = window.scrollY;
-    if (window.scrollY > prevScroll) {
-        document.getElementById("nav-bar").style.top = "-100px;";
-    }
-    else document.getElementById("nav-bar").style.top = "0";
-}
