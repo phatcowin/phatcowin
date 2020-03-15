ac4fa8324b06e3dd935ef5cb82295b766cd1647d
Dalton Cowin <dcowin1998@gmail.com>
2020-03-15 16:22:27 -0700

    Mobile update cont'd

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,20 @@
 var prevScrollPos = window.pageYOffset;
+var navToggled = document.getElementById("nav-mobile").style.top;
 
 window.onscroll = function () {
+	navToggled = document.getElementById("nav-mobile").style.top;
     // navigation
     var scrollPos = window.pageYOffset;
     if (prevScrollPos > scrollPos || scrollPos <= 0) {
         document.getElementById("nav-bar").style.transition = "opacity 0.4s ease-in, top 0.4s ease-out";
         document.getElementById("nav-bar").style.top = "0";
         document.getElementById("nav-bar").style.opacity = "1";
-        if (document.getElementById('nav-mobile').style.top !== "10px") document.getElementById("nav-mobile").style.top = "-265px"; // -370px
+        if (navToggled !== "5px") document.getElementById("nav-mobile").style.top = "-265px"; // -370px
     } else {
         document.getElementById("nav-bar").style.transition = "opacity 0.3s, top 0.5s";
         document.getElementById("nav-bar").style.top = "-90px";
         document.getElementById("nav-bar").style.opacity = "0";
-        if (document.getElementById('nav-mobile').style.top !== "10px") document.getElementById("nav-mobile").style.top = "-400px"; // -465px
+        if (navToggled !== "5px") document.getElementById("nav-mobile").style.top = "-400px"; // -465px
     }
 
     prevScrollPos = scrollPos;
@@ -20,13 +22,13 @@ window.onscroll = function () {
 }
 
 function toggle_m_nav() {
-    var navToggled = document.getElementById("nav-mobile").style.top;
+    navToggled = document.getElementById("nav-mobile").style.top;
     
-    if (navToggled == "30px") {
+    if (navToggled == "5px") {
         document.getElementById("nav-mobile").style.top = "-265px";
         document.getElementById("nav-mobile-blinds").style.opacity = "0";
     } else {
-        document.getElementById("nav-mobile").style.top = "30px";
+        document.getElementById("nav-mobile").style.top = "5px";
         document.getElementById("nav-mobile-blinds").style.opacity = "0.5";
     }
 }
